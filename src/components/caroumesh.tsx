438ff591dbe417bb058caf70295116ea7ad5216a
Adonis Stavridis <adonis.stavridis@gmail.com>
2021-08-25 16:57:57 +0200

    not functionnal yet! - mesh loading

--- src/components/caroumesh.tsx
+++ src/components/caroumesh.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
+import { Environment, OrbitControls, Stage } from '@react-three/drei';
 
 import { Model } from './model';
 
@@ -8,6 +8,7 @@ export interface CaroumeshProps {
   width?: number;
   height?: number;
   backgroundColor?: any;
+  src: string;
 }
 
 export function Caroumesh(props: CaroumeshProps) {
@@ -17,18 +18,33 @@ export function Caroumesh(props: CaroumeshProps) {
         width: props.width ? props.width : '100%',
         height: props.height ? props.height : '100%',
       }}
+      shadows
+      camera={{ fov: 45 }}
     >
-      <ambientLight intensity={0.1} />
-      <directionalLight color="white" position={[0, 0, 5]} />
-
       {props.backgroundColor ? (
         <color attach="background" args={props.backgroundColor} />
       ) : null}
-      <Model position={[-1.2, 0, 0]} />
-      <Model position={[1.2, 0, 0]} />
+      <fog attach="fog" args={['#101010', 10, 50]} />
+
+      <Suspense fallback={null}>
+        <Environment path="/cube" />
+        <Stage intensity={1} contactShadowOpacity={1} shadowBias={-0.0015}>
+          <Model src={props.src} />
+        </Stage>
+      </Suspense>
+
+      <mesh rotation-x={-Math.PI / 2} scale={100}>
+        <planeGeometry />
+        <meshStandardMaterial color="#101010" transparent depthWrite={false} />
+      </mesh>
+
+      <OrbitControls
+        autoRotate
+        enableZoom={false}
+        enablePan={false}
+        minPolarAngle={Math.PI / 2.8}
+        maxPolarAngle={Math.PI / 2.8}
+      />
     </Canvas>
   );
 }
-
-const rootElement = document.getElementById('root');
-ReactDOM.render(<Caroumesh />, rootElement);
--- src/components/model.tsx
+++ src/components/model.tsx
@@ -1,29 +1,19 @@
-import React, { useRef, useState } from 'react';
-import { useFrame } from '@react-three/fiber';
-import * as THREE from 'three';
+import React, { useLayoutEffect } from 'react';
+import { useGLTF } from '@react-three/drei';
 
-export function Model(props: any) {
-  const ref = useRef<THREE.Mesh>();
-  const [hovered, setHover] = useState(false);
-  const [active, setActive] = useState(false);
+export interface ModelProps {
+  src: string;
+}
 
-  useFrame(() => {
-    if (ref.current !== undefined) {
-      ref.current.rotation.y += 0.01;
-    }
-  });
+export function Model(props: ModelProps) {
+  const { scene } = useGLTF(props.src);
 
-  return (
-    <mesh
-      {...props}
-      ref={ref}
-      scale={active ? 1.5 : 1}
-      onClick={() => setActive(!active)}
-      onPointerOver={() => setHover(true)}
-      onPointerOut={() => setHover(false)}
-    >
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'tomato' : 'gold'} />
-    </mesh>
-  );
+  useLayoutEffect(() => {
+    scene.traverse(
+      (obj) =>
+        obj.type === 'Mesh' && (obj.receiveShadow = obj.castShadow = true)
+    );
+  }, [scene]);
+
+  return <primitive object={scene} {...props} />;
 }
