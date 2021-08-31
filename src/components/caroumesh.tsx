90b3c2401695c656ba9d362c8b10db004196fe79
Adonis Stavridis <adonis.stavridis@gmail.com>
2021-08-31 03:20:11 +0200

    playing around with postprocessing

--- src/components/caroumesh.tsx
+++ src/components/caroumesh.tsx
@@ -2,6 +2,8 @@ import React, { CSSProperties, Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { Html, useProgress } from '@react-three/drei';
 import { Color } from 'three';
+import { BrightnessContrast, EffectComposer, HueSaturation, SSAO } from '@react-three/postprocessing';
+import { BlendFunction } from 'postprocessing';
 
 type CaroumeshProps = {
   width?: number;
@@ -15,6 +17,35 @@ function Loader() {
   const { progress } = useProgress();
 
   return <Html center>{progress}% loaded</Html>;
+}
+
+function Effects() {
+  return (
+    <EffectComposer>
+      <SSAO
+        blendFunction={BlendFunction.MULTIPLY} // blend mode
+        samples={30} // amount of samples per pixel (shouldn't be a multiple of the ring count)
+        rings={4} // amount of rings in the occlusion sampling pattern
+        distanceThreshold={1.0} // global distance threshold at which the occlusion effect starts to fade out. min: 0, max: 1
+        distanceFalloff={0.0} // distance falloff. min: 0, max: 1
+        rangeThreshold={0.5} // local occlusion range threshold at which the occlusion starts to fade out. min: 0, max: 1
+        rangeFalloff={0.1} // occlusion range falloff. min: 0, max: 1
+        luminanceInfluence={0.9} // how much the luminance of the scene influences the ambient occlusion
+        radius={20} // occlusion sampling radius
+        scale={0.5} // scale of the ambient occlusion
+        bias={0.5} // occlusion bias
+      />
+      <HueSaturation
+        blendFunction={BlendFunction.NORMAL} // blend mode
+        hue={0} // hue in radians
+        saturation={0} // saturation in radians
+      />
+      <BrightnessContrast
+        brightness={0} // brightness. min: -1, max: 1
+        contrast={0} // contrast: min -1, max: 1
+      />
+    </EffectComposer>
+  );
 }
 
 export function Caroumesh(props: CaroumeshProps) {
@@ -47,6 +78,8 @@ export function Caroumesh(props: CaroumeshProps) {
         <pointLight color="white" intensity={0.3} position={[-50, -50, 50]} />
         {props.children}
       </Suspense>
+
+      <Effects />
     </Canvas>
   );
 }
