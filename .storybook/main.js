b7782e3c9671710f2fb6261e75ea1a05af6eb1d1
Adonis Stavridis <adonis.stavridis@gmail.com>
2021-10-17 16:41:04 +0200

    functionnal storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,5 +9,12 @@ module.exports = {
   // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
   typescript: {
     check: true, // type-check stories during Storybook build
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      compilerOptions: {
+        allowSyntheticDefaultImports: false,
+        esModuleInterop: false,
+      },
+    }
   }
 };
