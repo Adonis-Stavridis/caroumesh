f7aec54766f24c4f9f817bfb68e13918a9914718
Adonis-Stavridis <59580806+Adonis-Stavridis@users.noreply.github.com>
2023-08-24 13:57:03 +0000

    Deploying to gh-pages from @ Adonis-Stavridis/caroumesh@f46abf5e120a7fe93cb07f19a200e7a24272310a 🚀

---
+++ assets/syntaxhighlighter-4OVWVELQ-cca38daa.js
@@ -0,0 +1,2 @@
+import{S as n,c as d,s as f}from"./index-fb0cb95d.js";import"./iframe-e822d9c0.js";import"../sb-preview/runtime.js";import"./index-d475d2ea.js";import"./index-6bd1afc7.js";import"./_commonjsHelpers-de833af9.js";import"./index-d37d4223.js";import"./inheritsLoose-50ceb98b.js";import"./index-82885479.js";import"./index-356e4a49.js";export{n as SyntaxHighlighter,d as createCopyToClipboardFunction,f as default};
+//# sourceMappingURL=syntaxhighlighter-4OVWVELQ-cca38daa.js.map
--- lib/components/Scene/Scene.utils.d.ts
+++ lib/components/Scene/Scene.utils.d.ts
@@ -4,6 +4,6 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
 import { LoadedScene } from './Scene.types';
 export declare const useSceneLoader: (src: string) => LoadedScene;
-export declare const matchLoaderFromFileExtension: (src: string) => typeof GLTFLoader | typeof OBJLoader | typeof FBXLoader | never;
+export declare const matchLoaderFromFileExtension: (src: string) => typeof GLTFLoader | typeof OBJLoader | typeof FBXLoader;
 export declare const getSceneObject: (scene: LoadedScene, obj: Object3D) => Object3D<import("three").Event> | undefined;
 //# sourceMappingURL=Scene.utils.d.ts.map
